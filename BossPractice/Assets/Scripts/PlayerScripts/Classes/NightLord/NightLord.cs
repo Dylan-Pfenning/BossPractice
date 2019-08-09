38a6641490b29799b2baff84656c0e3d04128bad
Dylan-Pfenning <Dylan.pfenning@gmail.com>
2019-08-08 17:00:54 -0700

    Moved git ignore and added some scripts

---
+++ BossPractice/Assets/Scripts/PlayerScripts/Classes/NightLord/NightLord.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightLord : Player
+{
+    [SerializeField]
+    private GameObject _Star;
+    [SerializeField]
+    private float _StarSpeed = 10f;
+
+    private Vector3 SpawnLoc;
+    private Quaternion SpawnRot;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InputController myInput = GetComponent<InputController>();
+        myInput.SetMaxJumps(3);
+        myInput.SetAccel(6.0f);
+
+        //Setting Stats
+        Level = 220;
+
+        Luk = 22000;
+        Dex = 6000;
+        Str = 2000;
+        Int = 2000;
+
+        Attack = 450;
+        PerAtt = 24 * 3;
+        
+        SF = 210;
+        AF = 250;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //Quad throw throws 5 stars because of hyper skill over a period
+            SpawnLoc = transform.position;
+            SpawnRot = transform.rotation;
+            StartCoroutine("QuadThrow");
+        }
+    }
+    private void EndSkill(string name)
+    {
+        StopCoroutine(name);
+    }
+
+    #region QuadThrow
+
+    private void ThrowStars()
+    {
+        GameObject starThrown = Instantiate(_Star, SpawnLoc, SpawnRot);
+        Rigidbody starRB = starThrown.GetComponent<Rigidbody>();
+        Physics.IgnoreCollision(starThrown.GetComponent<SphereCollider>(), GetComponent<CapsuleCollider>());
+        starThrown.GetComponent<ThrowingStar>().Owner = this;
+        Debug.Log("Owner Set");
+        
+        starRB.velocity = transform.right * _StarSpeed;
+    }
+    private IEnumerator QuadThrow()
+    {
+        ThrowStars();
+        for (int i = 1; i < 5; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            ThrowStars();
+        }
+        EndSkill("QuadThrow");
+    }
+    #endregion
+}
---
+++ BossPractice/Assets/Scripts/PlayerScripts/Classes/NightLord/ThrowingStar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingStar : MonoBehaviour
+{
+    [SerializeField]
+    private float _lifetime = 5f;
+
+    public NightLord Owner;
+
+    private void Awake()
+    {
+        Destroy(gameObject, _lifetime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player") return;
+
+        if(other.gameObject.tag == "Enemy")
+        {
+            if(Owner == null)
+            {
+                Debug.LogWarning("Null Owner D:");
+                return;
+            }
+            //Deal damage to enemy here :)
+            //Check owners stats for damage formula same with enemy
+            
+        }
+    }
+}
